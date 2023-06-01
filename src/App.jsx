c80d0c5ec00986cd5bcc755bba5c2c18e80f0fc3
ANKIT <88872056+ankee7439@users.noreply.github.com>
2023-06-01 15:24:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useReducer } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold underline">Hello React</h1>
+    </>
+  );
+}
+
+export default App;
